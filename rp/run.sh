bd44f3c6c3d8ebc3ffbad1301ffbef5fb6b88ae9
Ritaban Pal <ritabanpal2000@gmail.com>
2026-09-25 02:59:20 +0530

    Delete rp directory

--- rp/run.sh
+++
@@ -1,9 +0,0 @@
-verilator --binary -j 0 --Wall full_adder.v rca.v rca_tb.v -top rca_tb --timing --CFLAGS "-std=c++20" --trace
-
-cd obj_dir
-
-make -f Vrca_tb.mk Vrca_tb
-
-./Vrca_tb
-
-gtkwave rca.vcd
